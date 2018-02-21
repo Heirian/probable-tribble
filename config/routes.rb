28c22609a87614d5efe45a52cd34fa0d1116ce74
Heirian <shinkcc@gmail.com>
2018-02-21 17:06:13 -0300

    Updates routes to works with I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'home#index'
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    devise_for :users
+    root to: 'home#index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
