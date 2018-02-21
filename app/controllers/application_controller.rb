d92de5d54b70cea902a8d815a2f1fd664d5e3cf1
Heirian <shinkcc@gmail.com>
2018-02-21 17:07:44 -0300

    Adds set locale method and default url options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+  before_action :default_url_options
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || current_user.try(:locale) || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
 end
