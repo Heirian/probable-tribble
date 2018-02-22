c458268bfebc75fa0dd8eda65b09c3b87da54206
Heirian <shinkcc@gmail.com>
2018-02-22 13:23:29 -0300

    Adds gravatar helper method

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module UsersHelper
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user, options = { size: 150, class: 'gravatar' })
+    size = options[:size]
+    img_class = options[:class]
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, size: size, class: img_class)
+  end
+end
