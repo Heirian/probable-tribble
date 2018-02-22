276400e2c9789a7e8efe17fe24c47d3662825f60
Heirian <shinkcc@gmail.com>
2018-02-22 18:22:58 -0300

    Adds profiles to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     devise_for :users
     root to: 'home#index'
+    resources :profiles
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
