8561d83dd2ce3eda3ef7a23140267d9b4833d45f
Heirian <shinkcc@gmail.com>
2018-02-22 18:32:05 -0300

    Adds a ensure profile method

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,8 +2,19 @@
 
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_profile, except: [:index]
 
   def index; end
 
   def show; end
+
+  private
+
+  def ensure_profile
+    @profile = Profile.find(ensure_profile_id)
+  end
+
+  def ensure_profile_id
+    params[:id].to_i
+  end
 end
