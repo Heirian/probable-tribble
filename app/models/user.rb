27f11ee37b56ac3db0cf6efdc19361c2c585ffeb
Heirian <shinkcc@gmail.com>
2018-02-25 14:32:36 -0300

    Adds ensure profile method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_one :profile, dependent: :nullify
+
+  after_create :ensure_profile
+
+  private
+
+  def auto_generate_name
+    email.split('@').first.capitalize
+  end
+
+  def ensure_profile
+    build_profile(name: auto_generate_name, bio: I18n.t(:autobio)).save
+  end
 end
