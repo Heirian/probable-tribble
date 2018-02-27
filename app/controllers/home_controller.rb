b2713649a6952a13ff41e931d52e0e0bad2a54c5
Heirian <shinkcc@gmail.com>
2018-02-26 22:50:34 -0300

    Adds a before authenticate method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,14 @@
 
 # This is the home controller which controls the home page
 class HomeController < ApplicationController
+  before_action :before_authenticate
   before_action :authenticate_user!
 
   def index; end
+
+  private
+
+  def before_authenticate
+    redirect_to new_user_session_path unless user_signed_in?
+  end
 end
