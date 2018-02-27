377b2c2c3e389d9940dbdcb06bad6a91a89062af
Heirian <shinkcc@gmail.com>
2018-02-26 22:49:24 -0300

    Updates sign out redirect path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,9 @@ class ApplicationController < ActionController::Base
   def default_url_options(options = {})
     { locale: I18n.locale }.merge options
   end
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
