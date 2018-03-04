83c7c74625a0c84d65ebc7e810aa36bfc36cff11
Heirian <shinkcc@gmail.com>
2018-03-04 13:04:05 -0300

    Adds default url options to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "gamarch_#{Rails.env}"
+  config.action_mailer.default_url_options = { host: ENV['MAILER_URL'] }
 
   config.action_mailer.perform_caching = false
 
