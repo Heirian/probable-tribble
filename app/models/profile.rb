4ba22e125eaca221e447674c878b22d45a5a1980
Heirian <shinkcc@gmail.com>
2018-03-24 22:41:50 -0300

    Fixes gender enum

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,5 +7,5 @@ class Profile < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 30 },
                    uniqueness: { case_sensitive: false }
 
-  enum gender: %I[#{I18n.t(:female)} #{I18n.t(:male)}]
+  enum gender: %i[female male]
 end
