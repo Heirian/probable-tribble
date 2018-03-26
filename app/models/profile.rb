fd613f45e72ef0e1f9ece26495e5b88e4f2e87c7
Heirian <shinkcc@gmail.com>
2018-03-25 23:42:56 -0300

    Changes position

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,11 +3,10 @@
 # This is the profile model
 class Profile < ApplicationRecord
   belongs_to :user
-
+  has_one_attached :avatar
   validates :name, presence: true, length: { minimum: 3, maximum: 30 },
                    uniqueness: { case_sensitive: false }
 
   enum gender: %i[female male]
 
-  has_one_attached :avatar
 end
