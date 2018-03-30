03ae7ec710b6ba29623468c2b7958f7797dce50f
Heirian <shinkcc@gmail.com>
2018-03-30 13:33:35 -0300

    Changes validate to username

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,10 +5,9 @@ class Profile < ApplicationRecord
   belongs_to :user
   accepts_nested_attributes_for :user
   has_one_attached :avatar
-  validates :name, presence: true, length: { minimum: 3, maximum: 30 },
+  validates :username, presence: true, length: { minimum: 3, maximum: 30 },
                    uniqueness: { case_sensitive: false }
   validates_with AvatarValidator
 
   enum gender: %i[female male]
-
 end
