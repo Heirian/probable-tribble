053f9004a5068167b01f377310a82eecd3e64980
Heirian <shinkcc@gmail.com>
2018-04-01 23:39:48 -0300

    Adds relationships to game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,8 @@
 class Game < ApplicationRecord
   translates :title, :description, :body
+  has_many :articles
+  has_many :game_developers
+  has_many :developers, through: :game_developers
+  has_many :game_genres
+  has_many :genres, through: :game_genres
 end
