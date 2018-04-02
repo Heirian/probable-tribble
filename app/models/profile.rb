b3f96d23db11a0df13ed504ff6c119a41bdec0d4
Heirian <shinkcc@gmail.com>
2018-04-01 23:38:07 -0300

    Adds has many articles to profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,7 @@
 # This is the profile model
 class Profile < ApplicationRecord
   belongs_to :user
+  has_many :articles
   accepts_nested_attributes_for :user
   has_many_attached :avatar
   validates :username, presence: true, length: { minimum: 3, maximum: 30 },
