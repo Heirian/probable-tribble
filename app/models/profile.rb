714d545a03fa6c662630def8dc8b7de7921d25f5
Heirian <shinkcc@gmail.com>
2018-04-20 21:51:44 -0300

    Adds communities and memberships relationships to profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,9 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_many :articles
+  has_many :memberships
+  has_many :communities, foreign_key: :owner_id
+  has_many :community_memberships, through: :memberships, source: :member
   accepts_nested_attributes_for :user
   has_many_attached :avatar
   validates :username, presence: true, length: { minimum: 3, maximum: 30 },
