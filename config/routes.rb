dc9028609e6e95ad23e20a6dcf61a1368d5e6f30
Heirian <shinkcc@gmail.com>
2018-04-28 03:44:16 +0000

    Adds communities and memberships routes

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
     resources :games, except: [:destroy]
     resources :categories, except: [:destroy]
     resources :articles
+    resources :community
+    resources :membership, only: %i[create update destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
