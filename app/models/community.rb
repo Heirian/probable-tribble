bc7d60c8c01b4458c42501196cbf34aa7852cb7e
Heirian <shinkcc@gmail.com>
2018-05-02 03:47:22 +0000

    Adds members methods to communities

--- app/models/community.rb
+++ app/models/community.rb
@@ -6,10 +6,52 @@ class Community < ApplicationRecord
 
   delegate :username, to: :owner
 
-  enum secrecy: %i[vip secret common]
-  enum kind: %i[strategy fun]
+  enum secrecy: %i[common vip secret]
+  enum kind: %i[pub strategy fun]
 
-  def game_title
-    game.title
+  delegate :title, to: :game, prefix: true
+
+  after_create :owner_membership
+
+  def accepted_member?(profile)
+    accepted_members.include?(profile)
+  end
+
+  def accepted_members
+    members.where(memberships: { approved: true })
+  end
+
+  def pending_members
+    members.where(memberships: { approved: false })
+  end
+
+  def managers_members
+    accepted_members.where(memberships: { status: manager_status })
+  end
+
+  def member?(profile)
+    members.include?(profile)
+  end
+
+  def manager?(profile)
+    accepted_member?(profile) && manager_status?(profile)
+  end
+
+  def manager_status?(profile)
+    membership(profile).status.in?(manager_status)
+  end
+
+  def membership(profile)
+    memberships.where(member_id: profile.id).last
+  end
+
+  private
+
+  def manager_status
+    ['administrator', 'moderator']
+  end
+
+  def owner_membership
+    memberships.create(member: owner, approved: true, status: 2)
   end
 end
