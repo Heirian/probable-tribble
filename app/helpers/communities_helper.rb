a4fabc415294657613949789fcc93ac1c31d57aa
Heirian <shinkcc@gmail.com>
2018-05-06 19:11:07 +0000

    Adds avatar related methods to communities helper

--- app/helpers/communities_helper.rb
+++ app/helpers/communities_helper.rb
@@ -1,6 +1,23 @@
 # frozen_string_literal: true
 
 module CommunitiesHelper
+  def community_avatar(community, options = { size: 150, class: 'gravatar' })
+    size = options[:size]
+    html_class = options[:class]
+
+    image_tag(avatar_url(community, size), size: size, class: html_class)
+  end
+
+  def avatar_url(community, size)
+    return avatar_fallback unless community.avatar.attached?
+
+    community.avatar.last.variant(resize: "#{size}x#{size}")
+  end
+
+  def avatar_fallback
+    asset_path('logoback.png')
+  end
+
   def join_or_leave(community)
     community.member?(current_profile) ? 'leave' : 'join'
   end
